209ae8bd4c560082ff23cbc57cc3315253199183
Militist <neumpanin@yandex.ru>
2024-12-01 21:22:36 +0300

    создана сущность Site

---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,28 @@
+package searchengine.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    private TextFields type;
+
+    @Column(name = "status_time")
+    private LocalDateTime statusTime; // Используем LocalDateTime
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(name = "url", columnDefinition = "VARCHAR(255)")
+    private String url;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(255)")
+    private String name
+}
