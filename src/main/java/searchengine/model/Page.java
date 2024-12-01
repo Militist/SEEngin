6c1a9547cf5770c73bb76220762fd87b925ea8d6
Militist <neumpanin@yandex.ru>
2024-12-01 21:16:21 +0300

    создана сущность Site

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,25 @@
+package searchengine.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pages", indexes = {
+        @Index(name = "idx_page_path", columnList = "path")
+})
+@Data
+public class Page {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "path")
+    private String path;
+
+    @Column(name = "code")
+    private int code;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+}
